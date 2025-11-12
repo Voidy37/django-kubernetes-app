a43e748c60df5ef4a8ac5c951319838220c2a1d6
Egor <113363110+NeverovEgor@users.noreply.github.com>
2025-11-12 21:36:33 +0300

    Update settings.py

--- django_app/settings.py
+++ django_app/settings.py
@@ -14,4 +14,28 @@ import socket
 def get_hostname():
  return socket.gethostname()
 # Добавить в контекст шаблонов или создать middleware
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.static',
+                # Добавляем наш кастомный контекст-процессор
+                'django_app.settings.pod_info_context',
+            ],
+        },
+    },
+]
 
+# Дополнительные настройки
+ALLOWED_HOSTS = ['*']
+DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
